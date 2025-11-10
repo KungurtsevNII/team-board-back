fa0d8b3eca538a1a1e5f97dc100643b1d8d58fca
zakhar <tolyati123@yandex.ru>
2025-11-10 23:19:11 +0400

    added sql req for check_board and create_board

--- src/repository/postgres/check_board.go
+++ src/repository/postgres/check_board.go
@@ -1,5 +1,37 @@
 package postgres
 
-func (r Repository) CheckBoard(shortName string) bool {
-	return false
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+)
+
+func (r Repository) CheckBoard(id string) bool {
+	uid, err := uuid.Parse(id)
+	if err != nil {
+		return false
+	}
+
+	tx, err := r.pool.BeginTx(context.TODO(), pgx.TxOptions{
+		IsoLevel: pgx.Serializable,
+	})
+	if err != nil {
+		return false
+	}
+	defer tx.Rollback(context.TODO())
+
+	var exists bool
+	err = tx.QueryRow(context.TODO(),
+		`SELECT EXISTS (SELECT 1 FROM boards WHERE id = $1 AND deleted_at IS NULL)`,
+		uid,
+	).Scan(&exists)
+
+	if err != nil {
+		return false
+	}
+
+	_ = tx.Commit(context.TODO())
+
+	return exists
 }
--- src/repository/postgres/create_board.go
+++ src/repository/postgres/create_board.go
@@ -1,10 +1,39 @@
 package postgres
 
-import "github.com/KungurtsevNII/team-board-back/src/domain"
+import (
+	"context"
+
+	"github.com/KungurtsevNII/team-board-back/src/domain"
+	"github.com/jackc/pgx/v5"
+)
 
 func (r Repository) CreateBoard(board domain.Board) (string, error) {
-	// board domain -> board record
-	// record раскаладывать в SQL
-	// SQL запрос отправить
-	return "1", nil
+	tx, err := r.pool.BeginTx(context.TODO(), pgx.TxOptions{
+		IsoLevel: pgx.Serializable,
+	})
+	if err != nil {
+		return "", err
+	}
+	defer tx.Rollback(context.TODO())
+
+	const sql = `
+		INSERT INTO boards (id, name, short_name, created_at, updated_at, deleted_at)
+		VALUES ($1, $2, $3, $4, $5, $6)
+	`
+	_, err = tx.Exec(context.TODO(), sql,
+		board.ID,
+		board.Name,
+		board.ShortName,
+		board.CreatedAt,
+		board.UpdatedAt,
+		board.DeletedAt,
+	)
+	if err != nil {
+		return "", err
+	}
+	if err = tx.Commit(context.TODO()); err != nil {
+		return "", err
+	}
+
+	return board.ID.String(), nil
 }
