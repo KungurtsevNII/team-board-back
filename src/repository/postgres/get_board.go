9c56f92bdd12070256f81d71cfea9fd90479d187
zakhar <tolyati123@yandex.ru>
2025-11-17 17:47:21 +0400

    modify get_boards

--- src/repository/postgres/get_board.go
+++ src/repository/postgres/get_board.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	"github.com/KungurtsevNII/team-board-back/src/domain"
+	"github.com/georgysavva/scany/v2/pgxscan"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 )
@@ -16,20 +17,10 @@ func (r Repository) GetBoard(ctx context.Context, ID string) (domain.Board, erro
 	}
 	var board domain.Board
 
-	//тут получаем доску
-	row := r.pool.QueryRow(ctx,
+	err = pgxscan.Get(ctx, r.pool, &board,
 		`SELECT id, name, short_name, created_at, updated_at, deleted_at 
-		FROM boards WHERE id = $1`,
-		uid,
-	)
+		FROM boards WHERE id = $1`, uid)
 
-	err = row.Scan(
-		&board.ID,
-		&board.Name,
-		&board.ShortName,
-		&board.CreatedAt,
-		&board.UpdatedAt,
-	)
 	if err != nil {
 		switch {
 		case errors.Is(err, pgx.ErrNoRows):
@@ -38,32 +29,14 @@ func (r Repository) GetBoard(ctx context.Context, ID string) (domain.Board, erro
 		return domain.Board{}, err
 	}
 
-	//тут получаем колонки доски
 	board.Columns = make([]domain.Column, 0)
-	rows, err := r.pool.Query(ctx, `
-	SELECT id, board_id, order_num, name 
-	FROM columns WHERE board_id = $1 
-	ORDER BY order_num;`, uid)
+	err = pgxscan.Select(ctx, r.pool, &board.Columns,
+		`SELECT id, board_id, order_num, name 
+		FROM columns WHERE board_id = $1 
+		ORDER BY order_num;`, uid)
 	if err != nil {
 		return domain.Board{}, err
 	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var column domain.Column
-		err := rows.Scan(
-			&column.ID,
-			&column.BoardID,
-			&column.OrderNum,
-			&column.Name,
-		)
-		if err != nil {
-			return domain.Board{}, err
-		}
-		board.Columns = append(board.Columns, column)
-	}
-
 	//TODO прикрутить задачи в колонки , сделаем как замерджим структуру task , чтобы не путать ветки
-
 	return board, nil
 }
